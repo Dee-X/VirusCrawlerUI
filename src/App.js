02e725b837195df5a1580ac67f651e8756d7c93d
Duncan <blackdj3497@gmail.com>
2022-10-25 21:17:11 -0500

    djb - Initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from '@mui/material/';
+import React from 'react';
+
+export const eel = window.eel
+eel.set_host( 'ws://localhost:8080' )
+
+// Expose the `sayHelloJS` function to Python as `say_hello_js`
+function sayHelloJS( x) {
+  console.log( 'Hello from ' + x )
+}
+// WARN: must use window.eel to keep parse-able eel.expose{...}
+window.eel.expose( sayHelloJS, 'say_hello_js' )
+
+// Test anonymous function when minimized. See https://github.com/samuelhwilliams/Eel/issues/363
+function show_log(msg) {
+  console.log(msg)
+}
+window.eel.expose(show_log, 'show_log')
+
+// Test calling sayHelloJS, then call the corresponding Python function
+sayHelloJS( 'Javascript World!' )
+eel.say_hello_py( 'Javascript World!' )
 
 function App() {
+  //const [count, setCount] = React.useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>TEST</code> and save to reload.
         </p>
+        <Button variant="contained" onClick={() => eel.pick_file2('C:\\Users\\Duncan\\Favorites')}>Click</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
